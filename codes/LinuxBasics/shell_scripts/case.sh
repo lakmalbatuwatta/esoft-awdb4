ad48bfef83f778dc2b0d67af0b7ae2775f5324be
Piusha Kalyana <piusha.k@softcodeit.com>
2017-08-19 13:26:52 +0530

    Case

---
+++ codes/LinuxBasics/shell_scripts/case.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+clear
+echo " ================== case esac=================="
+echo "Enter option"
+read -p "[ a,b,c,or x ] : " opt
+
+case "$opt" in
+    "a") echo " You have selected a "
+    ;;
+    "b") echo " You have selected b"
+    ;;
+    "c") echo " You have selected c"
+    ;;
+    "x") exit
+    ;;
+esac
